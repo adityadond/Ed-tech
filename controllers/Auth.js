6c01cbee1a497a83813fb3741f865b1a93b1909e
Aditya Dond <63242352+adityadond@users.noreply.github.com>
2023-08-23 09:59:39 +0530

    Completed till BE lec3

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,9 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //send otp
 
@@ -61,72 +64,216 @@ exports.sendOTP = async (req, res) => {
 //signup
 exports.signup = async (req, res) => {
   // data fetch from req body
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-    confirmPassword,
-    accountType,
-    contactNumber,
-    otp,
-  } = req.body;
-  //validate data
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      accountType,
+      contactNumber,
+      otp,
+    } = req.body;
+    //validate data
 
-  if (
-    !firstName ||
-    !lastName ||
-    !email ||
-    !password ||
-    !confirmPassword ||
-    !otp
-  ) {
-    return res.status(403).json({
-      success: false,
-      message: "All fields are required",
-    });
-  }
-
-  //2 password match
-  if (password !== confirmPassword) {
-    return res.status(400).json({
-      success: false,
-      message: "Password and confirm password does not match. Please try again",
-    });
-  }
-
-  //check user already exists
-  const existingUser = await User.findOne({email})
-  if(existingUser){
-    return res.status(400).json({
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !otp
+    ) {
+      return res.status(403).json({
         success: false,
-        message: "User already exists"
-    })
+        message: "All fields are required",
+      });
+    }
 
+    //2 password match
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Password and confirm password does not match. Please try again",
+      });
+    }
 
-}
+    //check user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
 
-  // find most recent otp for user
-  const recentOtp = await OTP.find({email}).sort({createdAt:-1}).limit(1)
-console.log(recentOtp,"recentOtp")
-  //validate OTP
-if(recentOtp.length==0){
-    return res.status(400).json({
-        success:false,
-        message:"OTP found"})
-}else if(otp!==recentOtp){
-    return res.status(400).json({
-        success:false,
-        message:"Invalid OTP"
-    })
-}
-  //hash passowrd
+    // find most recent otp for user
+    const recentOtp = await OTP.find({ email })
+      .sort({ createdAt: -1 })
+      .limit(1);
+    console.log(recentOtp, "recentOtp");
+    //validate OTP
+    if (recentOtp.length == 0) {
+      return res.status(400).json({
+        success: false,
+        message: "OTP found",
+      });
+    } else if (otp !== recentOtp) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+    //hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  //entry create in DB
+    //entry create in DB
+    const profileDetails = await Profile.create({
+      gender: null,
+      dateOfBirth: null,
+      about: null,
+      contactNumber: null,
+    });
 
-  // return respose
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      contactNumber,
+      accountType,
+      additionalDetails: profileDetails._id,
+      image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+    });
+
+    // return respose
+    return res.status(200).json({
+      success: true,
+      message: "User is registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "User cannot be registered. Please try again.",
+    });
+  }
 };
 
 //login
 
+exports.login = async (req, res) => {
+  try {
+    //get data from req body
+    const { email, password } = req.body;
+    //vadition of data
+    if (!email || !password) {
+      return res.status(403).json({
+        success: false,
+        message: "All fields are required. please try again",
+      });
+    }
+
+    // user check whether it exists or not
+    const user = await User.findOne({ email }).populate("additionalDetails");
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "USer is not registered, Please sign up first",
+      });
+    }
+
+    //generate jwt token after password matching
+    if (await bcrypt.compare(password, user.password)) {
+      const payload = {
+        email: user.email,
+        id: user._id,
+        role: user.accountType,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+      user.token = token;
+      user.password = undefined;
+
+      //create cookie and send response
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+      response.cookie("token", token, options).status(200).json({
+        success: true,
+        token,
+        user,
+        message: "Logged in successfully",
+      });
+    } else {
+      return response.status(401).json({
+        success: false,
+        message: "Password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    console.log(error);
+    return response.status(500).json({
+      success: false,
+      message: "Login failed, please try again",
+    });
+  }
+};
+
 //change password
+exports.changePassword = async (req, res) => {
+  //get datta from req body
+  //get oldpass,new pass and confirm pass
+  //validation
+  //validate pwd in db
+  //
+  //send mail password change
+  //return response
+  try {
+    const { oldPassword, newPassword, confirmPassword } = req.body;
+
+    // Validation
+    if (!oldPassword || !newPassword || !confirmPassword) {
+      return res.status(400).json({ message: 'All fields are required.' });
+    }
+
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({ message: 'New password and confirm password do not match.' });
+    }
+
+    // Fetch user from the database
+    const user = await User.findById(req.user.id); // Assuming you have user authentication middleware
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    // Validate old password
+    const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: 'Old password is incorrect.' });
+    }
+
+    // Hash and update the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10); // 10 is the salt rounds
+    user.password = hashedPassword;
+    await user.save();
+
+    // Send email notification for password change
+    await sendPasswordChangeEmail(user.email);
+
+    return res.status(200).json({ message: 'Password changed successfully.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'An error occurred while processing your request.' });
+  }
+
+};
---
+++ controllers/Tags.js
@@ -0,0 +1,55 @@
+const Tag = require("../models/Tags");
+
+//create Tag handler function
+
+exports.createTag = async (req, res) => {
+  try {
+    //fetch data
+    const { name, description } = req.body;
+
+    //validation
+    if (!name || !description) {
+      return res
+        .status(404)
+        .json({ success: false, message: "All fields are required" });
+    }
+    //create entry in db
+    const tagDetails = await Tag.create({
+      name: name,
+      description: description,
+    });
+    console.log(tagDetails, "tagDetails");
+    return res.status(200).json({
+      success: true,
+      message: "Tag Entry created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//getAllTags handler function
+
+exports.showAlltags = async (req, res) => {
+  try {
+    //fetch data
+    const allTags = await Tag.find({}, { name: true, description: true });
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "All tags returned successfully",
+        allTags,
+      });
+
+    //create entry in db
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
